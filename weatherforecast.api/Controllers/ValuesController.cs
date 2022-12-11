a3fafcf92df0c6b473aff95acac6197c62ed5113
adonis cruz villarraga <aoi.dev@hotmail.com>
2022-12-11 15:50:38 -0500

    added values controller and new settings section

---
+++ weatherforecast.api/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace weatherforecast.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var value = _configuration["values:value-1"];
+            await Task.CompletedTask;
+            return Ok(value);
+        }
+    }
+}
